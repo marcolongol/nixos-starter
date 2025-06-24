803324b96ad51eda6c9fc50fa40ddaebb5bcae6c
Lucas Marcolongo <lucas_marco@live.com>
2025-06-24 09:45:09 -0700

    chore(profile-desktop): enable audio and pulse support in PipeWire configuration

--- profiles/packages/desktop.nix
+++ profiles/packages/desktop.nix
@@ -51,9 +51,11 @@
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
+    audio.enable = true;
+    pulse.enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
-    pulse.enable = true;
+    jack.enable = true;
   };
 
   # Bluetooth
