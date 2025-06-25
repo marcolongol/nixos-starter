45b99bfc582f22683e907c00fd54e88a45b88b19
Lucas Marcolongo <lucas@marcolongo.dev>
2025-06-25 13:31:20 -0700

    chore(nixvim-lsp): add diagnostics

--- modules/nixvim/options.nix
+++ modules/nixvim/options.nix
@@ -48,4 +48,14 @@
   colorschemes.oxocarbon = {
     enable = true;
   };
+
+  diagnostic.settings = {
+    virtual_text = {
+      prefix = ""; # Custom prefix for virtual text diagnostics
+      spacing = 4; # Spacing between the diagnostic prefix and the text
+    };
+    signs = true; # Enable signs in the sign column
+    underline = true; # Underline diagnostics
+    update_in_insert = false; # Update diagnostics only in normal mode
+  };
 }
